8e2aad94b81d7a98d9edd24d40ba628721d434bc
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2018-08-09 21:20:47 -0500

    Using arrow functions

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  data = [{ firstName: "Mary", lastName: "Rose" }];
+  displayNames = [];
+  getNames = () => {
+    this.data.forEach(student => {
+      // read the properties and build strings
+      let fullName = '';
+      student.values().forEach(prop => {
+        fullName += prop + " ";
+      });
+      this.displayNames.push(fullName);
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React JS Project</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload the app.
-        </p>
+        { this.displayNames.forEach(name => {
+          return <div>${name}</div>;
+        })}
       </div>
     );
   }
